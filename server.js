c4c7f03c9d1f20fefa818b68ae9776c31095b7a3
hedera-s <irene.davydova@gmail.com>
2018-09-26 15:30:34 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,75 @@
 "use strict";
 
 let express = require("express");
+let cookieSession = require("cookie-session");
 let bodyparser = require("body-parser");
+let listModule = require("./listmodule.js");
 
 let server = express();
 server.use(express.static("public"));
-server.use(bodyparser.json());
 
-let listModule = require("./listmodule.js")
+
+server.use(cookieSession({
+	name: "session",
+	keys: ["key1", "key2"]
+}));
+
+server.use(bodyparser.json());
+server.use(bodyparser.urlencoded({extended: false}));
+
+// login 
+server.post("/login", (req,res) => {
+	let user = req.body.username,
+	pw = req.body.password;
+	if (user === "u1" && pw === "123") {
+	req.session.user = "u1";
+	res.redirect("/app.html");
+	} else if (user === "u2" && pw === "123") {
+		req.session.user = "u2";
+		res.redirect("/app.html");
+	} else {
+		res.redirect("/");
+	}
+	console.log(req.session.user + " ist angemeldet");
+	
+});
+
+
+
+
+let checkAuth = (req,res,next) => {
+	if(!req.session.user) {
+		res.redirect("/");
+	} else {
+		return next();
+	}
+};
+// 
+
+	
+	
+server.get("/app", checkAuth, (req,res) => {
+	res.redirect("/app.html");
+	
+});
+
+server.get("/app.html",(req,res) => {
+	if(!req.session.user) {
+		res.end();
+	}
+});
+
+server.get("/logout", (req,res) => {
+	console.log(req.session.user + " ist abgemeldet");
+	delete req.session.user;
+	res.redirect("/");
+})
+
+
+
+
+
+
 
 let myList = new listModule.ShopingList();
 myList.add("Äpfel");
@@ -51,7 +113,8 @@ server.delete("/items", (req,res) => {
     myList.clean();
     console.log("Gekauftes gelöscht");
     res.send();
-})
+});
+
 
 
 server.listen(80);
